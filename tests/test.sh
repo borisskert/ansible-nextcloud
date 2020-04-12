6b6683fa1c92a11513c00f5317c089ad71155742
Boris Skert <boris.skert@gmail.com>
2020-04-12 19:48:00 +0200

    Run add-missing-indices via daily timer and convert-filecache-bigint after nextcloud stop

--- tests/test.sh
+++ tests/test.sh
@@ -12,11 +12,11 @@ ansible-playbook -i inventory.ini test.yml \
   && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
 
-./wtfc -T 120 -S 0 -I 2 curl -f http://192.168.33.84:10081
+./wtfc -T 300 -S 0 -I 2 curl -f http://192.168.33.84:10081
 
-curl -s http://192.168.33.84:10081 \
- | grep -q '</html>' \
+curl -o /dev/null -s -w "%{http_code}\n" http://192.168.33.84:10081 \
+ | grep -qE '302|200' \
   && (echo 'curl test: pass' && exit 0) \
   || (echo 'curl test: fail' && exit 1)
 
-#vagrant destroy -f
+vagrant destroy -f
